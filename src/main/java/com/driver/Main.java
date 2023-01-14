ccbbcd1b453386ace063ede535828dec23bbf5c4
unknown <rohini.khandare16598@gmail.com>
2023-01-14 22:47:31 +0530

    adding public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,12 +7,12 @@ public class Main {
 //        System.out.println(obj.meth());
         obj.meth();
     }
-    static class A{
+     public static class A{
         String meth(){
             return "Invoking method from class A";
         }
     }
-     static class B extends A{
+     public static class B extends A{
         @Override
         String meth() {
             return "Method is overridden in Extendend class B";
